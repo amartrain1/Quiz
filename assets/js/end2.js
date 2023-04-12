625a3bf16fc2af800f2a386b49b813a9178e30f7
Alex Martrain <alexander.martrain.2020@stpauls.com>
2023-04-12 10:00:44 -0500

    i didnt fix all the bugs ;-;

--- assets/js/end2.js
+++ assets/js/end2.js
@@ -33,7 +33,7 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.href = 'index.html';
+    return window.location.href = 'index.html';
 
-    console.log(highScores);
+    // console.log(highScores);
 };
